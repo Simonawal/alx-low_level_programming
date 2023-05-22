9aa5dc23c46adf012735bd60306a365a568124dd
mohamed <medmramer@gmail.com>
2023-05-21 18:32:25 -0700

    variables_if_else_while

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,4 @@
+julien@ubuntu:~/0x01$ gcc -Wall -pedantic -Werror -Wextra -std=gnu89 2-print_alphabet.c -o 2-print_alphabet
+julien@ubuntu:~/0x01$ ./2-print_alphabet 
+abcdefghijklmnopqrstuvwxyz
+julien@ubuntu:~/0x01$
---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,3 @@
+julien@ubuntu:~/0x01$ gcc -Wall -pedantic -Werror -Wextra -std=gnu89 3-print_alphabets.c -o 3-print_alphabets
+julien@ubuntu:~/0x01$ ./3-print_alphabets | cat -e
+
